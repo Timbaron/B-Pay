5624ed3814688530ae8413041f57d6de3f924183
Akiode Timothy Adewale <timothy.akiode@studemt.aul.edu.ng>
2021-01-19 08:01:00 +0000

    Add Middlerware to Dashboard controller

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class Dashboard extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         return view('Dashboard');
