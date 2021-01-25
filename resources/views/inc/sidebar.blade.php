cbe7558d8e005a61bc4f31ef8d3d4488ae41bbca
Akiode Timothy Adewale <timothy.akiode@studemt.aul.edu.ng>
2021-01-25 10:03:39 +0000

    Sidebar

---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,29 @@
+<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 mb-20">
+    <div class="card mb-30">
+        <div class="card-header">
+            Someone to pay?
+        </div>
+        <div class="card-body">
+            <p>Boluptate quis optio, quibusdam quo unde cum dolores tempora veniam eius, voluptatum illum</p>
+            <a href="#" class="bttn-small btn-fill"><i class="ti-user"></i> Make a payment</a>
+        </div>
+    </div>
+    <div class="card mb-30">
+        <div class="card-header">
+            Earn $25 to invite
+        </div>
+        <div class="card-body">
+            <p>Boluptate quis optio, quibusdam quo unde cum dolores tempora veniam eius, voluptatum illum</p>
+            <a href="#" class="bttn-small btn-fill"><i class="ti-arrow-down"></i> refer a friend</a>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            Receive a payment?
+        </div>
+        <div class="card-body">
+            <p>Boluptate quis optio, quibusdam quo unde cum dolores tempora veniam eius, voluptatum illum</p>
+            <a href="#" class="bttn-small btn-fill"><i class="ti-direction"></i> Receive payment</a>
+        </div>
+    </div>
+</div>
